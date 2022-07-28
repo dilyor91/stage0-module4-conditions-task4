a6a75815126505656b2069aa9d819515703a8c0b
dilyor91 <dilyor_05@mail.ru>
2022-07-28 12:29:06 +0500

    Task #4. Ternary Conditions

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,8 @@
 package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
-    public void printGreatest(int first, int second, int third) {
+    public static void printGreatest(int first, int second, int third) {
+        int result = first>second?(first>third?first:third):(second>third?second:third);
+        System.out.println(result);
     }
 }
